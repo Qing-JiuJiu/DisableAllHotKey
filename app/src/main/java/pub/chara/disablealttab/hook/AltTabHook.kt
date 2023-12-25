d9002c28d526dff47da2270fee154e83832a9a66
Qing-JiuJiu <77143400+Qing-JiuJiu@users.noreply.github.com>
2023-12-25 09:46:06 +0800

    Update AltTabHook.kt

--- app/src/main/java/pub/chara/disablealttab/hook/AltTabHook.kt
+++ app/src/main/java/pub/chara/disablealttab/hook/AltTabHook.kt
@@ -7,17 +7,13 @@ import de.robv.android.xposed.XposedBridge
 object AltTabHook : BaseHook() {
     override fun init() {
         try {
-            //disable alt-tab
+            //disable hot-key
             //this works for any android version
             findMethod("com.android.server.policy.PhoneWindowManager") {
                 name == "interceptKeyBeforeDispatching"
             }.hookBefore { param ->
                 run {
-                    val arg1: KeyEvent = param.args[1] as KeyEvent;
-                    // alt-tab
-                    if ((arg1.isAltPressed && arg1.keyCode == 61)) {
-                        param.result = 0L;
-                    }
+                    param.result = 0L;
                 }
             }
             XposedBridge.log("DisableAltTab: AltTabHook success!")
